442acafa21475e493f9bdcf03b66f0de17ac3f56
consuelaziza <consuelavlek@me.com>
2020-05-18 13:10:35 +0200

    import Pokemonex

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,11 +6,9 @@ export default function Pokemon() {
     <div>
       <h2>Pokemon name: {"Charizard"}</h2>
       <p>Weight: {90} kg</p>
-      <p>Awesome: {true}</p>
-      <p>Terrifying: {false}</p>
-      <p>What about null? {null}</p>
-      <p>And undefined? {undefined}</p>
-      <p>Types: {{ fire: true, water: false }}</p>
+      <p>Awesome: {true ? "YES!" : "nope, not really"}</p>
+      <p>Terrifying: {false ? "Very" : "nah, lovable"}</p>
+      <p>Abilities: {["Blaze", "Solar power", "Tough claws", "Drought"]}</p>
       
     </div>
   );
