74e5f3658b0d7b9e53f980912e2d02d7f88f6391
consuelaziza <consuelavlek@me.com>
2020-05-18 16:38:05 +0200

    import Pokemon map6

--- src/App.js
+++ src/App.js
@@ -8,35 +8,36 @@ function App() {
     <div className="App">
        <main>
         <Title content="Pokemon" />
-
-        <Pokemon 
-        name="Charizard"
-        weight="90"
-        awesome="yes"
-        terrifying="no"
-        abilities="Blaze, Solar power, Tough claws, Drought"
-      />
-         <Pokemon
-        name="Bulbasaur"
-        weight="6.9"
-        awesome="yes"
-        terrifying="no"
-        abilities="Overgrow, Chlorophyll"
-      />
-      <Pokemon
-         name="Mewtwo"
-         weight="122"
-         awesome="yes"
-         terrifying="yes"
-         abilities="Pressure, Unnerve, Steadfast, Insomnia"
-      />
-      <Pokemon
-         name="Mega beedrill"
-         weight="65"
-         awesome="no"
-         terrifying="yes"
-         abilities="Intimidate, Unnerve"
-      />
+        const all_pokemon = [
+          {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"]
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"]
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"]
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"]
+  }
+];
       </main>
     </div>
   );
